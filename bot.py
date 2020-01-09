7dac4499a3a3744f7b5c013658cd63af41787756
VictorLCT952 <57416797+VictorLCT952@users.noreply.github.com>
2020-01-09 22:53:00 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Streaming(name="First Class Gaming", url="https://www.twitch.tv/monstercat"))
+    print('Logged in as {0} ({0.id})'.format(client.user))
+    print('-----------------------------------------------')
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined the server.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server.')
+
+@client.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount=3):
+    await ctx.channel.purge(limit=amount)
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(' ✅ Pinging')
+    await ctx.send(f'Ping is {round(client.latency * 1000)} MS.')
+
+@client.event
+async def on_member_join(member):
+    role = discord.utils.get(member.guild.roles, name="Member")
+    await member.add_roles(role)
+
+@client.command()
+async def say(ctx, *, msg):
+    await ctx.message.delete()
+    await ctx.send("{}" .format(msg))
+
+
+client.run(os.getenv('TOKEN'))
