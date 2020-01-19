7aad0094e43411644aa9f31c666f6a3be4759c25
VictorLCT952 <57416797+VictorLCT952@users.noreply.github.com>
2020-01-19 11:26:50 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,10 +11,6 @@ async def on_ready():
     print('-----------------------------------------------')
 
 @client.event
-async def on_member_join(member):
-    print(f'{member} has joined the server.')
-
-@client.event
 async def on_member_remove(member):
     print(f'{member} has left the server.')
 
@@ -27,4 +23,16 @@ async def on_member_join(member):
 async def invite(ctx):
     await ctx.send('https://discord.gg/8GuPrQ8')
  
+@client.event
+async def on_member_join(member):
+    embed = discord.Embed(colour=0x95efcc, description=f"Welcome to First Class Gaming! You are the {len(list(member.guild.members))} member!", timestamp=datetime.datetime.utcfromtimestamp(1553629094))
+    embed.set_thumbnail(url=f"{member.avatar_url}")
+    embed.set_author(name=f"{member.name}", icon_url=f"{member.avatar_url}")
+    embed.set_footer(text=f"{member.guild}", icon_url=f"{member.guild.icon_url}")
+    embed.timestamp = datetime.datetime.utcnow()
+
+    channel = client.get_channel(id=668385254561677322)
+
+    await channel.send(embed=embed)
+
 client.run(os.getenv('TOKEN'))
