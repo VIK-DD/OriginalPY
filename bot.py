c5207b9c41c86ea8eb1b69396e7c0a34fcb37110
VictorLCT952 <57416797+VictorLCT952@users.noreply.github.com>
2020-02-02 21:29:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,11 +3,10 @@ from discord.ext import commands
 import os
 import datetime
 
-client = commands.Bot(command_prefix = '*')
+client = commands.Bot(command_prefix = '\\')
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Streaming(name="@RubyBoT", url="https://www.twitch.tv/ninja"))
     print('Logged in as {0} ({0.id})'.format(client.user))
     print('-----------------------------------------------')
 
@@ -20,19 +19,28 @@ async def on_member_join(member):
     role = discord.utils.get(member.guild.roles, name="Member")
     await member.add_roles(role)
     
-@client.command()
-async def invite(ctx):
-    await ctx.send('https://discord.gg/8GuPrQ8')
  
 @client.event
 async def on_member_join(member):
-    embed = discord.Embed(colour=0x95efcc, description=f"Welcome to First Class Gaming! You are the {len(list(member.guild.members))} member!", timestamp=datetime.datetime.utcfromtimestamp(1553629094))
+    embed = discord.Embed(colour=0x95efcc, description=f"Welcome to xSupportGamingx! You are the {len(list(member.guild.members))} member!", timestamp=datetime.datetime.utcfromtimestamp(1553629094))
     embed.set_thumbnail(url=f"{member.avatar_url}")
     embed.set_author(name=f"{member.name}", icon_url=f"{member.avatar_url}")
     embed.set_footer(text=f"{member.guild}", icon_url=f"{member.guild.icon_url}")
     embed.timestamp = datetime.datetime.utcnow()
 
-    channel = client.get_channel(id=668385254561677322)
+    channel = client.get_channel(id=673581023166070814)
+
+    await channel.send(embed=embed)
+
+@client.event  
+async def on_member_remove(member):
+    embed = discord.Embed(colour=0x95efcc, description=f"Just left our server, We are now {len(list(member.guild.members))} member!", timestamp=datetime.datetime.utcfromtimestamp(1553629094))
+    embed.set_thumbnail(url=f"{member.avatar_url}")
+    embed.set_author(name=f"{member.name}", icon_url=f"{member.avatar_url}")
+    embed.set_footer(text=f"{member.guild}", icon_url=f"{member.guild.icon_url}")
+    embed.timestamp = datetime.datetime.utcnow()
+
+    channel = client.get_channel(id=673581059086090261)
 
     await channel.send(embed=embed)
 
