07b46d57a298793c2df17d536028669bb1d69be5
VictorLCT952 <57416797+VictorLCT952@users.noreply.github.com>
2020-03-07 12:57:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ client = commands.Bot(command_prefix = '!')
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.idle, activity=discord.Game("Beta {!}"))
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game("Support"))
     print('Logged in as {0} ({0.id})'.format(client.user))
     print('-----------------------------------------')
 
